c9e7f88ad1da065699b5b1ae771ffefb2fef0286
yunyoungsik <yunyoungsik91@gmail.com>
2023-10-31 11:23:12 +0900

    2023.10.31.1123

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -30,6 +30,10 @@
 </template>
 
 <script>
+import bg01 from "../assets/img/card_bg01.jpg"
+import bg02 from "../assets/img/card_bg02.jpg"
+import bg03 from "../assets/img/card_bg03.jpg"
+
 export default {
     props: {
         props: "string",
@@ -40,19 +44,19 @@ export default {
         return {
             cardInfo: [
                 {
-                    img: "../assets/img/card_bg01.jpg",
+                    img: bg01,
                     // img: "/src/assets/img/card_bg01.jpg",
                     title: '웹표준 사이트 만들기',
                     desc: '사이트를 만들기 위한 기초 강의입니다. 기본 코딩에 대한 감각을 익히고, 웹 표준을 준수하면서 코딩하는 방법을 익힙니다. 다양한 예제를 통해 사이트를 만드는',
                 },
                 {
-                    img: "../assets/img/card_bg02.jpg",
+                    img: bg02,
                     // img: "/src/assets/img/card_bg02.jpg",
                     title: '반응형 사이트 만들기',
                     desc: '웹표준 사이트를 완성하고, 반응형을 익히는 사이트입니다. 기본 코딩을 익히고, 반응형 감각을 키우면 모바일 및 반응형 사이트도 만들 수 있습니다. 웹표준 사이트를',
                 },
                 {
-                    img: "../assets/img/card_bg03.jpg",
+                    img: bg03,
                     // img: "/src/assets/img/card_bg03.jpg",
                     title: '패랠렉스 사이트 만들기',
                     desc: '기업 사이트 중 50% 이상이 패랙렉스 스크로링 효과로 만들어져 있습니다. 웹 표준, 반응형 사이트를 완성하였다면, 이제는 인터랙티브한 패랠렉스 스크롤링 사이트를',
